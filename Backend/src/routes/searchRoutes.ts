a9cded0d51cbb2b3dd758ab8ab0fe50998a9ae56
tuấn anh nguyễn <tuananhgicungduoc@gmail.com>
2026-05-28 09:39:10 +0700

    Định tuyến API tìm kiếm

--- Backend/src/routes/searchRoutes.ts
+++ Backend/src/routes/searchRoutes.ts
@@ -1,1 +1,10 @@
 
+import { Router } from "express";
+import { search } from "../controllers/searchController.js";
+import { verifyToken as protect } from "../middleware/authMiddleware.js";
+
+const router = Router();
+
+router.get("/", protect, search);
+
+export default router;
